1c6a3d9475e1e26647258e2003273f42d9378c20
DattolaFrancesco <dattolafrancescoo@gmail.com>
2026-05-22 12:08:38 +0200

    fixed globe responsivness

--- src/app/(main)/components/Globe.jsx
+++ src/app/(main)/components/Globe.jsx
@@ -3,11 +3,15 @@ import { useEffect, useRef, useState } from 'react'
 import maplibregl from 'maplibre-gl'
 import 'maplibre-gl/dist/maplibre-gl.css'
 
-export default function Globe() {
+export default function Globe({ searchParams }) {
   const [spot,setSpot] = useState(null)
+  const [windowWidthCustom, setWindowWidthCustom] = useState(null)
+  const mapInstance = useRef(null)
   const mapRef = useRef(null)
    async function getSpot(){
-    const url = `http://localhost:3003/spots/all?`;
+    const params = await searchParams
+    const query = new URLSearchParams(params)  
+    const url = `http://localhost:3003/spots/all?${query.toString()}`;
     try
     { const res = await fetch(url,{
         method:"GET",
@@ -24,74 +28,50 @@ export default function Globe() {
         console.log(error.message)
     }
    }
+  const getZoom = () => {
+    if (window.innerWidth < 480) return 0.8   
+    if (window.innerWidth < 1024) return 1   
+    if (window.innerWidth < 1280) return 1.2   
+    if (window.innerWidth < 1480) return 1.3   
+    return 1.8
+}
    useEffect(()=>{
     getSpot()
+    console.log(".")
    },[])
+   useEffect(() => {
+    setWindowWidthCustom(window.innerWidth)
+    const handleResize = () => setWindowWidthCustom(window.innerWidth)
+    window.addEventListener('resize', handleResize)
+    return () => window.removeEventListener('resize', handleResize)
+}, [])
+useEffect(() => {
+    if (!mapInstance.current) return
+    if (!mapInstance.current.isStyleLoaded()) return
+    mapInstance.current.jumpTo({ zoom: getZoom() })
+}, [windowWidthCustom])
   useEffect(() => {
     if (!mapRef.current) return
-
-    const map = new maplibregl.Map({
+    mapInstance.current = new maplibregl.Map({
       container: mapRef.current,
       style: `https://api.maptiler.com/maps/dataviz-dark/style.json?key=WvVUBxCG2IWaZflw6QsZ`,
-      zoom: 1.8,
-      minZoom: 1.6,
+      zoom: getZoom(),
+      minZoom: 0.8,
       center: [10, 40],
       attributionControl: false,
     })
 
-    map.on('style.load', () => {
-      map.setProjection({ type: 'globe' })
-      map.setPaintProperty('background', 'background-color', '#0c0c0c')
-      map.setPaintProperty('countries-fill', 'fill-color', '#1c1c1c')
-      map.setPaintProperty('countries-boundary', 'line-color', '#2a2a2a')
-      map.setFog({
-        color: '#000000',
-        'high-color': '#000000',
-        'horizon-blend': 0.1,
-        'space-color': '#000000',
-        'star-intensity': 0.3,
-      })
+    mapInstance.current.on('style.load', () => {
+      mapInstance.current.setProjection({ type: 'globe' })
+      mapInstance.current.setPaintProperty('background', 'background-color', '#0c0c0c')
+      mapInstance.current.setPaintProperty('countries-fill', 'fill-color', '#1c1c1c')
+      mapInstance.current.setPaintProperty('countries-boundary', 'line-color', '#2a2a2a')
     })
-
-    map.on('load', () => {
-      const markers = spot.map(city => {
-        const el = document.createElement('div')
-        el.style.cssText = `
-          width: 6px;
-          height: 6px;
-          background: #ffffff;
-        `
-        const tooltip = document.createElement('div')
-        tooltip.style.cssText = `
-        position: absolute;
-        bottom: 14px;
-        left: 50%;
-        transform: translateX(-50%);
-        color: #fff;
-        font-size: 11px;
-        opacity: 0;
-        `
-      tooltip.textContent = city.name
-      el.appendChild(tooltip)
-      el.addEventListener('mouseenter', () => {
-      tooltip.style.opacity = '1'
-      })
-    el.addEventListener('mouseleave', () => {
-  tooltip.style.opacity = '0'
-      })
-        const marker = new maplibregl.Marker({ element: el, anchor: 'center',opacityWhenCovered: '0' })
-          .setLngLat([city.longitude, city.latitude
-])
-          .addTo(map)
-        return marker
-      })
-    })
-
-    return () => map.remove()
+    return () => mapInstance.current.remove()
   }, [spot])
 
   return (
-    <div className="aspect-square w-[45%] rounded-full overflow-hidden">
+    <div className="aspect-square w_custom_globe rounded-full overflow-hidden">
       <div ref={mapRef} className="w-full h-full" />
     </div>
   )
--- src/app/(main)/page.jsx
+++ src/app/(main)/page.jsx
@@ -3,7 +3,7 @@ import Globe from "./components/Globe";
 
 export default function Map() {
   return (
-    <div className="flex justify-center items-center">
+    <div className="flex justify-center items-center h-full">
       <Globe/>
     </div>
   )
