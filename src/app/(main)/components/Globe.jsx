31f80b8a163a844deeb76d4452ee078654dfedf0
DattolaFrancesco <dattolafrancescoo@gmail.com>
2026-05-22 12:43:26 +0200

    adds spot marker to the globe

--- src/app/(main)/components/Globe.jsx
+++ src/app/(main)/components/Globe.jsx
@@ -1,15 +1,22 @@
 'use client'
-import { useEffect, useRef, useState } from 'react'
+import { useCallback, useEffect, useRef, useState } from 'react'
 import maplibregl from 'maplibre-gl'
 import 'maplibre-gl/dist/maplibre-gl.css'
 
+
 export default function Globe({ searchParams }) {
+  // const 
   const [spot,setSpot] = useState(null)
   const [windowWidthCustom, setWindowWidthCustom] = useState(null)
   const mapInstance = useRef(null)
   const mapRef = useRef(null)
-   async function getSpot(){
+  const [mapReady, setMapReady] = useState(false)
+
+  //fetch get spot come funziona usecallback?
+   const getSpot = useCallback(async()=>{
+    console.log(searchParams)
     const params = await searchParams
+    console.log(params)
     const query = new URLSearchParams(params)  
     const url = `http://localhost:3003/spots/all?${query.toString()}`;
     try
@@ -20,56 +27,94 @@ export default function Globe({ searchParams }) {
     }
     })
     const data = await res.json();
-    console.log(data)
-    setSpot(data.content)
     if (!res.ok) throw new Error(data.message);
+     setSpot(data.content)
+     console.log(data.content)
     }
     catch(error){
         console.log(error.message)
     }
-   }
-  const getZoom = () => {
-    if (window.innerWidth < 480) return 0.8   
-    if (window.innerWidth < 1024) return 1   
-    if (window.innerWidth < 1280) return 1.2   
-    if (window.innerWidth < 1480) return 1.3   
+   },[searchParams])
+  // function usecallback thanks to that is not going to re rendere every time
+  const getZoom = useCallback(() => {
+    if (windowWidthCustom < 480) return 0.8   
+    if (windowWidthCustom < 1024) return 1   
+    if (windowWidthCustom < 1280) return 1.2   
+    if (windowWidthCustom < 1480) return 1.5   
     return 1.8
-}
-   useEffect(()=>{
-    getSpot()
-    console.log(".")
-   },[])
+  },[windowWidthCustom])
    useEffect(() => {
-    setWindowWidthCustom(window.innerWidth)
-    const handleResize = () => setWindowWidthCustom(window.innerWidth)
-    window.addEventListener('resize', handleResize)
-    return () => window.removeEventListener('resize', handleResize)
-}, [])
-useEffect(() => {
-    if (!mapInstance.current) return
-    if (!mapInstance.current.isStyleLoaded()) return
-    mapInstance.current.jumpTo({ zoom: getZoom() })
-}, [windowWidthCustom])
-  useEffect(() => {
-    if (!mapRef.current) return
-    mapInstance.current = new maplibregl.Map({
-      container: mapRef.current,
-      style: `https://api.maptiler.com/maps/dataviz-dark/style.json?key=WvVUBxCG2IWaZflw6QsZ`,
-      zoom: getZoom(),
-      minZoom: 0.8,
-      center: [10, 40],
-      attributionControl: false,
-    })
+      getSpot()
+      setWindowWidthCustom(window.innerWidth)
+      const handleResize = () => setWindowWidthCustom(window.innerWidth)
+      window.addEventListener('resize', handleResize)
+      return () => window.removeEventListener('resize', handleResize)
+    }, [getSpot])
+   useEffect(() => {
+      if (!mapInstance.current) return
+      if (!mapInstance.current.isStyleLoaded()) return
+      mapInstance.current.jumpTo({ zoom: getZoom() })
+    } , [windowWidthCustom])
+   useEffect(() => {
+      if (!mapRef.current) return
+      mapInstance.current = new maplibregl.Map({
+        container: mapRef.current,
+        style: `https://api.maptiler.com/maps/dataviz-dark/style.json?key=WvVUBxCG2IWaZflw6QsZ`,
+        zoom: getZoom(),
+        minZoom: 0.8,
+        center: [10, 40],
+        attributionControl: false,
+      })
 
     mapInstance.current.on('style.load', () => {
       mapInstance.current.setProjection({ type: 'globe' })
       mapInstance.current.setPaintProperty('background', 'background-color', '#0c0c0c')
       mapInstance.current.setPaintProperty('countries-fill', 'fill-color', '#1c1c1c')
       mapInstance.current.setPaintProperty('countries-boundary', 'line-color', '#2a2a2a')
+      setMapReady(true)
     })
     return () => mapInstance.current.remove()
-  }, [spot])
-
+    }, [])
+   useEffect(()=>{
+    if(!spot || !mapReady) return
+    if(mapInstance.current.getSource('spots')){
+      mapInstance.current.getSource('spots').setData({
+        type: 'FeatureCollection',
+        features: spot.map(s => ({
+        type: 'Feature',
+        geometry: {
+            type: 'Point',
+            coordinates: [s.longitude, s.latitude]
+        },
+        properties: { name: s.name }
+    }))
+    })
+    }else{
+          mapInstance.current.addSource('spots', {
+          type: 'geojson',
+          data: {
+          type: 'FeatureCollection',
+          features: spot.map(s => ({
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [s.longitude, s.latitude]
+            },
+            properties: { name: s.name }
+            }))
+    }
+    })
+    }
+    mapInstance.current.addLayer({  
+    id: 'spots-layer',
+    type: 'circle',
+    source: 'spots',
+    paint: {
+        'circle-radius': 4,
+        'circle-color': '#ffffff'
+    }
+  })
+   },[mapReady,spot])
   return (
     <div className="aspect-square w_custom_globe rounded-full overflow-hidden">
       <div ref={mapRef} className="w-full h-full" />
--- src/app/(main)/page.jsx
+++ src/app/(main)/page.jsx
@@ -1,10 +1,10 @@
 import Image from "next/image";
 import Globe from "./components/Globe";
 
-export default function Map() {
+export default function Map({searchParams}) {
   return (
     <div className="flex justify-center items-center h-full">
-      <Globe/>
+      <Globe searchParams={searchParams}/>
     </div>
   )
 }
