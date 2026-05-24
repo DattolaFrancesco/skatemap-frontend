42a42402c28339dce643ddd952663ab00b1c5266
DattolaFrancesco <dattolafrancescoo@gmail.com>
2026-05-24 19:10:05 +0200

    adds higlight link in the nav bar to the right pathname

--- src/app/(main)/components/NavLinks.jsx
+++ src/app/(main)/components/NavLinks.jsx
@@ -1,11 +1,14 @@
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function NavLinks({params}){
+  const pathname = usePathname()
+  console.log(pathname)
     return(
     <div className="flex flex-col right justify-between items-end gap-2">
       <div className="flex gap-1">
-        <Link className="nav-link" href={`/grid?${params?.toString()}`}>Grid</Link>
-        <Link className="nav-link" href={`/?${params?.toString()}`}>Map</Link>
+        <Link className={`nav-link ${pathname === "/grid"?"bg-black/40!":""}`} href={`/grid?${params?.toString()}`}>Grid</Link>
+        <Link className={`nav-link ${pathname === "/"?"bg-black/40!":""}`} href={`/?${params?.toString()}`}>Map</Link>
       </div>
 
       <div className="flex gap-1">
