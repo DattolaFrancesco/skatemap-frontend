4bae4e677d1dada10ae1f0314e740b11dac048e2
DattolaFrancesco <dattolafrancescoo@gmail.com>
2026-05-24 21:23:03 +0200

    put login in right angol and globe place holder in th middle with h-full

--- src/app/(main)/loading.jsx
+++ src/app/(main)/loading.jsx
@@ -1,6 +1,6 @@
 export default function Loading() {
   return (
-    <div className="w-full flex justify-center items-center">
+    <div className="w-full h-full flex justify-center items-center">
       <div className="aspect-square w_custom_globe rounded-full overflow-hidden">
         <div className="w-full h-full rounded-full bg-[#1a1a1a] animate-pulse flex items-center justify-center">
           <div className="w-3/4 h-3/4 rounded-full border border-white/5" />
