c9e7e898f1088287ee45715ab26bb1516b89a382
DattolaFrancesco <dattolafrancescoo@gmail.com>
2026-05-24 13:51:49 +0200

    delated nav skeleton from loading in dashboard

--- src/app/(account)/dashboard/loading.jsx
+++ src/app/(account)/dashboard/loading.jsx
@@ -1,29 +1,4 @@
-function NavSkeleton() {
-  return (
-    <div className="w-full flex flex-col md:flex-row">
-      <div className="w-full flex flex-col gap-3">
-        <div className="h-14 w-48 bg-black/20 animate-pulse" />
-        <div className="h-7 w-36 bg-black/20 animate-pulse mt-3" />
-        <div className="h-7 w-52 bg-black/20 animate-pulse" />
-      </div>
-      <div className="w-full flex flex-col justify-around">
-        <div className="flex ms-auto">
-          <div className="nav-link w-16 h-5 bg-black/20 animate-pulse" />
-        </div>
-        <div className="flex justify-end gap-3 py-4">
-          <div className="flex flex-col justify-center items-center gap-2">
-            <div className="h-14 w-12 bg-black/20 animate-pulse" />
-            <div className="h-5 w-14 bg-black/20 animate-pulse" />
-          </div>
-          <div className="flex flex-col justify-center items-center gap-2">
-            <div className="h-14 w-12 bg-black/20 animate-pulse" />
-            <div className="h-5 w-20 bg-black/20 animate-pulse" />
-          </div>
-        </div>
-      </div>
-    </div>
-  )
-}
+
 
 function LinksSkeleton() {
   return (
@@ -70,7 +45,6 @@ function SpotsSkeleton() {
 export default function Loading() {
   return (
     <div className="px-2 py-1">
-      <NavSkeleton />
       <LinksSkeleton />
       <SpotsSkeleton />
     </div>
