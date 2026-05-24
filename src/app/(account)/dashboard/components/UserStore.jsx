9dcc829fd4c2637a43301e8fd8710b0e71fcbc4a
DattolaFrancesco <dattolafrancescoo@gmail.com>
2026-05-24 16:28:12 +0200

    add loading page all spot in dash board and allo the filters for the new all spot path

--- src/app/(account)/dashboard/components/UserStore.jsx
+++ src/app/(account)/dashboard/components/UserStore.jsx
@@ -3,6 +3,8 @@ const useUserStore = create((set)=>({
     user: null,
     setUser: (data) => set({ user: data }),
     refresh: true,
-    setRefresh: (data) => set({ refresh: data })
+    setRefresh: (data) => set({ refresh: data }),
+    pendingSpots: true,
+    setPendingSpots: (data) => set({ pendingSpots: data })
 }))
 export default useUserStore;
