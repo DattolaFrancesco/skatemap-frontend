7dbb98bdb42f796d22eeb836b2560920e9a9dd91
DattolaFrancesco <dattolafrancescoo@gmail.com>
2026-05-25 15:08:34 +0200

    adds waiting for the bot message

--- src/app/(main)/components/ChatBot.jsx
+++ src/app/(main)/components/ChatBot.jsx
@@ -4,6 +4,7 @@ import { ChevronUp } from 'lucide-react';
 import {  useState , useRef, useEffect} from "react";
 import TextareaAutosize from "react-textarea-autosize";
 import useChatStore from './ChatStore';
+import { Send } from 'lucide-react';
 
 
 export default function ChatBot(){
@@ -13,6 +14,7 @@ export default function ChatBot(){
     const [lastPrompt, setLastPrompt] = useState("")
     const [openChat, setOpenChat] = useState(false)
     const [loading, setLoading] = useState(false)
+    const [loadingMessage, setLoadingMessage] = useState(false)
     const chatEndRef = useRef(null)
     async function getChat(){
     const url = `${process.env.NEXT_PUBLIC_API_URL}/bot/test`;
@@ -22,7 +24,8 @@ export default function ChatBot(){
               headers: { 'Content-Type': 'application/json' },
               body: JSON.stringify(lastPrompt)
         })
-        if(!res.ok) throw new Error("bot not available")
+        if(!res.ok) throw new Error("the bot is skating right now!, try later")
+        setLoadingMessage(false)
         const reader = res.body.getReader() // reading the data stream
         const decoder = new TextDecoder() //convert bit in string
         let fullResponse = ""
@@ -35,10 +38,12 @@ export default function ChatBot(){
         return fullResponse;
     }
     catch(err){
-        console.log(err.message)
+        setLoadingMessage(false)
+        return err.message
     }}
     async function handleSubmit(e){
         setLoading(true)
+        setLoadingMessage(true)
         e.preventDefault()
         setChat(prev=>[...prev,{role:"user",message:lastPrompt}])
         setLastPrompt("")
@@ -57,19 +62,20 @@ export default function ChatBot(){
         <div className="relative z-[9999]">
                 <button onClick={()=>setOpenChat(!openChat)} className="w-full text-start flex">
                     Say something <span className='ms-auto'>{!openChat ? <ChevronDown/>: <ChevronUp/>}</span></button>
-            <div className={`${openChat?"h-[300px] md:h-[500px] border bg-white opacity-100":"h-0 opacity-0"} absolute min-w-[250px] sm:min-w-full flex flex-col justify-between 
+            <div className={`${openChat?"h-[300px] md:h-[500px] border bg-white opacity-100":"h-0 opacity-0"} absolute min-w-[250px] sm:w-full flex flex-col justify-between 
             overflow-hidden transition-all duration-300  p-1 mt-1`}>
                 <div ref={chatEndRef} className="w-full overflow-y-scroll py-3">
                     {chat && chat?.map((m,index)=>(<p key={index} className={` wrap-break-word p-2 text-sm w-4/5 m-1 
                         ${m.role === "bot"?"bg-black/20 me-auto":"ms-auto bg-black/50"} rounded-sm`}>{m.message}</p>))}
                      {lastResponse.length>0 && <p className="bg-black/20 wrap-break-word p-2 text-sm w-4/5 m-1 me-auto rounded-sm">{lastResponse}</p>}
+                     {loadingMessage && <p className="bg-black/20 wrap-break-word p-2 text-sm w-4/5 m-1 me-auto rounded-sm animate-pulse">...</p>}
                 </div>
               <form className={`flex gap-1 ${loading?"animate-pulse":""}`} onSubmit={handleSubmit}>
                 <TextareaAutosize minRows={1} value={lastPrompt} placeholder='have a chat!' onChange={(e)=>{setLastPrompt(e.target.value)}}
                 onKeyDown={(e)=>{
                     if(e.key === "Enter")handleSubmit(e)}}
-                className="w-full rounded-sm placeholder:px-1  placeholder:text-sm  focus:outline-none focus:ring-0 resize-none border bg-black/10"/>
-                <button type='submit' disabled={loading} className={`rounded-sm`}>Send</button>
+                className="w-full rounded-sm placeholder:py-0.5  placeholder:text-sm  focus:outline-none focus:ring-0 resize-none border bg-black/10 px-1"/>
+                <div className='flex items-end'><button type='submit' disabled={loading} className={`rounded-sm h-fit p-1`}><Send size={20}/></button></div>
               </form>
             </div>
         </div>
