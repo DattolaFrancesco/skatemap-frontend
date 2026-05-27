e0b36197dcb873b902399b665a4dd955af7d8b52
DattolaFrancesco <dattolafrancescoo@gmail.com>
2026-05-27 20:57:49 +0200

    adds some gsap to the navbar

--- src/app/(main)/components/NavBar.jsx
+++ src/app/(main)/components/NavBar.jsx
@@ -22,9 +22,15 @@ export default function NavBar(){
     const [search,setSearch] = useState(null)
     const [filterOpen,setFilterOpen] = useState(false)
     const [openFilter, setOpenFilter] = useState(null)
+    const tlRef = useRef(null)
+    const tlContinentsRef = useRef(null) 
+    const tlTypesRef = useRef(null) 
+    const tlRiskRef = useRef(null) 
     const inputRef = useRef(null)
     const router = useRouter()
     const containerGenericFiltersRef = useRef(null)
+    const [filterIsReady, setFilterIsReady] = useState(true)
+    const [btnIsReady, setBtnIsReady] = useState(true)
     const multipleSelection = (category,f)=>{
          setSelected(prev=>({
             ...prev,
@@ -48,47 +54,131 @@ export default function NavBar(){
 }, [selected, router,search])
     useGSAP(()=>{
         const genericFilters = document.querySelectorAll(".genericFilter")
-        gsap.set(genericFilters,{xPercent:-200, opacity:0})
+        const continents = gsap.utils.toArray(document.querySelectorAll(".continents"))
+        const types = gsap.utils.toArray(document.querySelectorAll(".types"))
+        const risks = gsap.utils.toArray(document.querySelectorAll(".risks"))
+        gsap.set(genericFilters,{xPercent:-300, opacity:0})
+        gsap.set(types,{opacity:0})
+        gsap.set(risks,{x:43, y:0, opacity:0})
+        gsap.set(continents,{x:-300, opacity:0})
     },{scope:containerGenericFiltersRef})
+    useGSAP(()=>{
+        console.log(openFilter)
+        tlTypesRef.current?.kill() 
+        tlContinentsRef.current?.kill()
+         closeAll()
+        switch(openFilter){
+            case "Location":
+            openLocation()
+            break
+            case "Type":
+            openType()
+            break
+            case "Risk":
+            openRisk()
+            break
+            default:
+            break
+
+        }
+    },{scope:containerGenericFiltersRef, dependencies:[]})
+
+    function closeAll(){
+        tlTypesRef.current?.reverse()
+        tlContinentsRef.current?.reverse()
+        tlRiskRef.current?.reverse()
+    }
+    function openLocation(){
+        if(openFilter !== "Location"){
+            tlContinentsRef.current?.reverse()
+        }else{
+            tlContinentsRef.current?.kill() 
+            const continents = gsap.utils.toArray(document.querySelectorAll(".continents"))
+            if(continents){
+            tlContinentsRef.current = gsap.timeline({onComplete:()=>{setBtnIsReady(true)},onReverseComplete: () =>setBtnIsReady(true)}) 
+            tlContinentsRef.current
+                    .to(continents, {x:75, duration:0.3})
+                    .to(continents?.[0], {x:75,opacity: 1, duration:0.1})
+                    .to(continents?.slice(1), {
+                    x: 75,
+                    opacity: 1,
+                    y: (i) => (i + 1) * 21,
+                    duration: 0.2,
+                    stagger: 0.1
+                })
+                }
+        }
+    }
+    function openType(){
+        if(openFilter !== "Type"){
+            tlTypesRef.current?.reverse()
+        }else{
+            tlTypesRef.current?.kill() 
+            const types = gsap.utils.toArray(document.querySelectorAll(".types"))
+            if(types){
+            tlTypesRef.current = gsap.timeline({onComplete:()=>{setBtnIsReady(true)},onReverseComplete: () =>setBtnIsReady(true)}) 
+            tlTypesRef.current
+                    .to(types, { duration:0.3})
+                    .to(types?.[0], {opacity: 1, duration:0.1})
+                    .to(types?.slice(1), {
+                    opacity: 1,
+                    duration: 0.2,
+                    stagger: 0.1
+                })
+                }
+        }
+    }
+    function openRisk(){
+        if(openFilter !== "Risk"){
+            tlRiskRef.current?.reverse()
+        }else{
+            tlRiskRef.current?.kill() 
+            const risks = gsap.utils.toArray(document.querySelectorAll(".risks"))
+            if(risks){
+            tlRiskRef.current = gsap.timeline({onComplete:()=>{setBtnIsReady(true)},onReverseComplete: () =>setBtnIsReady(true)}) 
+            tlRiskRef.current
+                    .to(risks, {x:43, duration:0.3})
+                    .to(risks?.[0], {x:43,opacity: 1, duration:0.1})
+                    .to(risks?.slice(1), {
+                    x: 43,
+                    opacity: 1,
+                    duration: 0.2,
+                    stagger: 0.1
+                })
+                }
+        }
+    }
     function openGenerics(){
+        setFilterIsReady(false)
+        tlRef.current?.kill() 
         const genericFilters = document.querySelectorAll(".genericFilter")
         const els = gsap.utils.toArray(genericFilters)
         if(!filterOpen){
-            gsap.to(els[0], {
-                xPercent:0,
-                opacity: 1 
-            })
-            gsap.to(els.slice(1),{
-                xPercent:0,
-                opacity:0
-            })
-            gsap.to(els.slice(1),{
-                xPercent:0,
-                opacity:0,
-                onComplete:()=>{
-                    els.slice(1).forEach((f,i)=>{
-                        if(i == 0) gsap.to(f,{yPercent:110,opacity:1})
-                        else gsap.to(f,{yPercent:220,opacity:1})
-                    })
-                }
-            })
-        }
-        else {
-            gsap.to(genericFilters,{xPercent:-200,opacity:0,stagger:0.2,duration:0.1})
-        }
-
+           tlRef.current =  gsap.timeline({onComplete:()=>{setFilterIsReady(true)},onReverseComplete: () => setFilterIsReady(true)}) 
+           tlRef.current
+                .to(els, {xPercent:0, duration:0.1})
+                .to(els[0], {xPercent:0,opacity: 1, duration:0.1})
+                .to(els?.slice(1), {
+                xPercent: 0,
+                opacity: 1,
+                yPercent: (i) => (i + 1) * 110,
+                duration: 0.2,
+                stagger: 0.1
+            },)
+            }
+            else tlRef.current?.reverse()
     }
     return(
        <div className="z-10">
         <nav className="navbar p-2">
-        <section className="left flex flex-col h-full">
+        <section className="left flex flex-col h-full ">
             {/* input search div */}
             <div><input ref={inputRef} type="text" placeholder="Search" onChange={(e)=>setSearch(e.currentTarget.value)} className="w-full"/></div>
-            <aside className="flex gap-0.5 pt-1">
+            <aside className="flex gap-0.5 pt-1 ">
                     {/* filter main div  */}
                    <div>
                     <button  
-                    className={`${!filterOpen?null :"bg-primary-500"}`}
+                    className={`${!filterOpen?null :"bg-primary-500"} ${filterIsReady ? "":"disabled-btn"}`}
                     onClick={()=>{
                     setFilterOpen(!filterOpen)
                     setOpenFilter(null)
@@ -102,49 +192,51 @@ export default function NavBar(){
                             <div className="relative" >
                                 <button
                                     data-label="Location"
-                                    className={`w-fit absolute ${openFilter == null || openFilter !== "Location"   ? null : "bg-primary-500"} genericFilter`}
-                                    onClick={()=>{setOpenFilter(openFilter === "Location"?null :"Location")}}>Location
+                                    className={`w-fit absolute ${openFilter == null || openFilter !== "Location"   ? null : "bg-primary-500"} 
+                                    ${btnIsReady ? "":" disabled-btn bg-red-400"} genericFilter`}
+                                    onClick={()=>{
+                                        setOpenFilter(openFilter === "Location"?null :"Location")
+                                        setBtnIsReady(false)
+                                        }}>Location
                                 </button>
                                 <button 
                                 data-label="Type"
-                                className={`w-fit absolute ${openFilter == null || openFilter !== "Type"   ? null : "bg-primary-500"} genericFilter`}
-                                onClick={()=>{setOpenFilter(openFilter === "Type"?null :"Type")}}>Type
+                                className={`w-fit absolute ${openFilter == null || openFilter !== "Type"   ? null : "bg-primary-500"} 
+                                ${btnIsReady ? "":" disabled-btn bg-red-700"} genericFilter`}
+                                onClick={()=>{
+                                    setOpenFilter(openFilter === "Type"?null :"Type")
+                                    setBtnIsReady(false)
+                                    }}>Type
                                 </button>
                                 <button 
                                 data-label="Risk"
-                                className={`w-fit absolute ${openFilter == null || openFilter !== "Risk"   ? null : "bg-©"} genericFilter`} 
-                                onClick={()=>{setOpenFilter(openFilter === "Risk"?null :"Risk")}}>Risk
+                                className={`w-fit absolute ${openFilter == null || openFilter !== "Risk"   ? null : "bg-primary-500"} 
+                                ${btnIsReady ? "":" disabled-btn bg-red-700"} genericFilter`}
+                                onClick={()=>{
+                                    setOpenFilter(openFilter === "Risk"?null :"Risk")
+                                    setBtnIsReady(false)
+                                    }}>Risk
                                 </button>
-                                {/* <div 
-                                className={`absolute flex flex-col gap-0.5 top-0 left-[103%] ${openFilter === "Location" ? "" : "invisible"}`}>
                                    {filters.location.map((f,index)=>(
                                     <button key={index} onClick={()=>multipleSelection("location",f)} 
-                                    className={selected.location.includes(f)?"bg-primary-500":""}
+                                    className={`${selected.location.includes(f)?"bg-primary-500":""} continents absolute`}
                                     >{f}</button>
                                    ))}
-                                </div>  */}
-                            </div>
-                            <div className="relative">
-
-                                <div
-                                    className={`absolute flex flex-col gap-0.5 top-0 left-[103%] ${openFilter === "Type" ? "" : "invisible"}`}>
-                                    {filters.type.map((f,index)=>(
-                                        <button key={index} onClick={()=>multipleSelection("type",f)}
-                                        className={selected.type.includes(f)?"bg-primary-500":""}
-                                        >{f}</button>
-                                    ))}
+                                 <div className="flex flex-col gap-0.5 absolute left-[75%] top-[36%]">
+                                        {filters.type.map((f,index)=>(
+                                            <button key={index} onClick={()=>multipleSelection("type",f)}
+                                            className={`${selected.type.includes(f)?"bg-primary-500":""} types`}
+                                            >{f}</button>
+                                        ))}
+                                 </div>
+                                <div className="flex flex-col gap-0.5">
+                                        {filters.risk.map((f,index)=>(
+                                            <button key={index} onClick={()=>multipleSelection("risk",f)}
+                                            className={`${selected.risk.includes(f)?"bg-primary-500":""} risks `}
+                                            >{f}</button>
+                                        ))}
                                 </div>
-                            </div>
-                            <div className="relative">
-                                <div 
-                                className={`absolute flex flex-col gap-0.5 top-0 left-[103%] ${openFilter === "Risk" ? "" : "invisible"}`}>
-                                    {filters.risk.map((f,index)=>(
-                                        <button key={index} onClick={()=>multipleSelection("risk",f)}
-                                        className={selected.risk.includes(f)?"bg-primary-500":""}
-                                        >{f}</button>
-                                    ))}
-                                </div>
-                            </div>
+                        </div>
                        </div>
                     </div>
             </aside>
--- src/app/(main)/components/NavLinks.jsx
+++ src/app/(main)/components/NavLinks.jsx
@@ -10,14 +10,14 @@ export default function NavLinks({params}){
     return(
     <div className="flex flex-col right justify-between items-end gap-2">
       <div className="flex gap-1">
-        <TransitionLink className={`nav-link ${pathname === "/grid"?"bg-primary-500":""} ${statusHref?" disabled-btn":""}`} href={`/grid?${params?.toString()}`}>Grid</TransitionLink>
-        <TransitionLink className={`nav-link ${pathname === "/"?"bg-primary-500":""}  ${statusHref?" disabled-btn":""}`} href={`/?${params?.toString()}`}>Map</TransitionLink>
+        <TransitionLink className={`nav-link ${pathname === "/grid"?"bg-primary-500 disabled-btn":""} ${statusHref?" disabled-btn":""}`} href={`/grid?${params?.toString()}`}>Grid</TransitionLink>
+        <TransitionLink className={`nav-link ${pathname === "/"?"bg-primary-500 disabled-btn":""}  ${statusHref?" disabled-btn":""}`} href={`/?${params?.toString()}`}>Map</TransitionLink>
       </div>
 
       <div className="flex flex-wrap gap-1 justify-end">
-        <TransitionLink className={`nav-link ${pathname === "/donate"?"bg-primary-500":""}  ${statusHref?" disabled-btn":""}`} href={`/donate?${params?.toString()}`}>Donate</TransitionLink>
+        <TransitionLink className={`nav-link ${pathname === "/donate"?"bg-primary-500 disabled-btn":""}  ${statusHref?" disabled-btn":""}`} href={`/donate?${params?.toString()}`}>Donate</TransitionLink>
         <TransitionLink className={`nav-link ${statusHref?" disabled-btn":""}`} href={`/dashboard`}>Profile</TransitionLink>
-        <TransitionLink className={`nav-link ${pathname === "/login"?"bg-primary-500":""}  ${statusHref?" disabled-btn":""}`} href={`/login?${params?.toString()}`}>Login</TransitionLink>
+        <TransitionLink className={`nav-link ${pathname === "/login"?"bg-primary-500 disabled-btn":""}  ${statusHref?" disabled-btn":""}`} href={`/login?${params?.toString()}`}>Login</TransitionLink>
       </div>
 
     </div>
