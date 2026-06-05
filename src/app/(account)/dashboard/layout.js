bcd6adf9b0cfac668fcd4221ca5fc9552de8209f
DattolaFrancesco <dattolafrancescoo@gmail.com>
2026-06-05 21:21:17 +0200

    resized account page

--- src/app/(account)/dashboard/layout.js
+++ src/app/(account)/dashboard/layout.js
@@ -3,12 +3,12 @@ import InfoUser from "./components/InfoUser";
 import LinkDashboard from "./components/LinkDashboard";
 export default function DashBoardLayout({ children, searchParams }) {
   return (
-      <div className="w-full min-h-dvh flex flex-col p-3 overflow-x-hidden">
+      <div className="flex flex-col h-dvh landscape:h-auto landscape:min-h-dvh relative">
         <div className="h-[230px] shrink-0 flex flex-col justify-between">
           <InfoUser/>
           <LinkDashboard searchParams={searchParams}/>
         </div>
-        <div className="w-full flex-1">{children}</div>
+        <div className="flex-1 overflow-y-auto overscroll-none landscape:overflow-visible">{children}</div>
       </div>
   );
 }
