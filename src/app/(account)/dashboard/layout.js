e11b34ef64c459c95fb4751a8b3bd40536a0c36f
DattolaFrancesco <dattolafrancescoo@gmail.com>
2026-06-30 15:26:01 +0200

    added suspande dashboard

--- src/app/(account)/dashboard/layout.js
+++ src/app/(account)/dashboard/layout.js
@@ -1,13 +1,19 @@
 import "@/app/globals.css"
+import { Suspense } from "react"
 import InfoUser from "./components/InfoUser";
 import OpenMedia from "@/app/(main)/components/OpenMedia";
+
 export default function DashBoardLayout({ children, searchParams }) {
   return (
-      <div className="flex flex-col h-dvh landscape:h-auto landscape:min-h-dvh  overflow-hidden relative">
+      <div className="flex flex-col h-dvh landscape:h-auto landscape:min-h-dvh overflow-hidden relative">
         <div className="shrink-0 flex px-3 pt-3">
           <InfoUser searchParams={searchParams}/>
         </div>
-        <div className="flex-1 overflow-y-auto overscroll-none landscape:overflow-visible flex flex-col">{children}</div>
+        <div className="flex-1 overflow-y-auto overscroll-none landscape:overflow-visible flex flex-col">
+          <Suspense fallback={null}>
+            {children}
+          </Suspense>
+        </div>
         <OpenMedia/>
       </div>
   );
